f3ce5454e60f5543360f70501ee6c05fc363b624
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2022-01-30 21:34:24 -0300

    :bug: trying fix deploy connection

--- backend/build/models/connection.js
+++ backend/build/models/connection.js
@@ -8,7 +8,10 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://127.0.0.1:27017';
 const DB_NAME = 'lexart-fullstack';
-const connection = () => mongodb_1.MongoClient.connect(MONGO_DB_URL)
+const connection = () => mongodb_1.MongoClient.connect(MONGO_DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
     .then(connection => connection.db(DB_NAME))
     .catch(() => {
     process.exit(1);
--- frontend/src/api/configApi.ts
+++ frontend/src/api/configApi.ts
@@ -1,7 +1,4 @@
 import axios from 'axios';
-// import { config } from 'dotenv';
-
-// config();
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3001',
