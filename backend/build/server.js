a3a3700a8d7b736f778d1bc1bb3985361b4e1b70
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2022-02-03 11:24:52 -0300

    fix deploy

--- backend/build/server.js
+++ backend/build/server.js
@@ -17,7 +17,6 @@ app.use(express_1.default.json());
 app.use('/register/mercadoLivre', registerRouterMercadoLivre_1.default);
 app.use('/register/buscape', registerRouterBuscape_1.default);
 app.use(errorHandler_1.default);
-exports.default = app;
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}! 🏆`);
 });
